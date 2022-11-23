2e5be5e4ed28b7ca33e601d0b1df9dc294bb61eb
WeerapatMatee <56258343+Account-Weerapat@users.noreply.github.com>
2022-11-23 20:19:02 +0700

    push

--- python/py02.py
+++ python/py02.py
@@ -3,19 +3,11 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-    # driver.get("https://finviz.com/forex.ashx")
- # Iterate over each table row
-
-#  table-light
-
 
 try:
     driver.get("https://finviz.com/forex_performance.ashx")
-    # Get the table
     table = driver.find_element(By.CLASS_NAME, "table-light")
-    # Iterate over each table row
     for row in table.find_elements(By.TAG_NAME, "tr"):
-        # list comprehension to get each cell's data and filter out empty cells
         row_data = list(filter(None, [td.text for td in row.find_elements(By.TAG_NAME, "td")]))
         if row_data == []:
             continue
